c389da78ec028387534bff2389ccbc6b72aec8d2
Justin <jli208@hawk.iit.edu>
2020-08-23 09:52:07 -0500

    removed mock webpage

--- webpage/AMQ_files/battleRoyalPlayer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*exported BattleRoyalePlayer*/
-
-class BattleRoyalePlayer extends GamePlayer {
-	constructor(name, level, gamePlayerId, host, avatarInfo) {
-		super(name, level, gamePlayerId, host, avatarInfo);
-		this.inGame = true;
-	}
-}
